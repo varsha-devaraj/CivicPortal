79920b1cb20f6247c46d7580a9cfaff28907bf0d
Varshitha CD <varshithacd@gmail.com>
2026-09-19 09:41:29 +0530

    Update backend and frontend

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 // Core Middlewares
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
